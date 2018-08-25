eb21efc375ece0f3be7f54ebe30f7e4aca371a9a
katerren <katekrivets@me.com>
2018-08-26 00:23:13 +0300

    map added

---
+++ src/components/Map.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCLgxZfnB0Z_jW23hZqWbpnR4LU9TW9oQ0&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `100vh` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={props.zoom} defaultCenter={props.center}>
+    {props.isMarkerShown && (
+      <Marker position={{ lat: -34.397, lng: 150.644 }} />
+    )}
+    {console.log(props)}
+  </GoogleMap>
+));
+
+export default MyMapComponent;
