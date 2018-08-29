7d8cfca3007a4b3e6a79fcf11a1fc5225dbc2a68
katerren <katekrivets@me.com>
2018-08-29 03:50:15 +0300

    last fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,9 +9,10 @@ class Search extends Component {
     }
     render() {
         return ( 
-            <div className="search" aria-label="Search input">
+            <div className="search" aria-label="search">
                 <DebounceInput
                     tabIndex="2"
+                    aria-label="search input"
                     placeholder="Search by name or category..."
                     minLength={2}
                     debounceTimeout={1000}
